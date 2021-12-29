74ea4328b509bcd52d1692747311f3001717cfc2
raoulkr <raoulkr@live.nl>
2021-12-29 16:07:52 +0100

    adding controller

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/day-1', [PagesController::class, 'day1']);
+Route::get('/day-2', [PagesController::class, 'day2']);
+Route::get('/day-4', [PagesController::class, 'day4']);
