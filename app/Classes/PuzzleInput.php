cc55d8e203752f852b197f6d514b2f9f3f835451
raoulkr <raoulkr@live.nl>
2021-12-30 20:02:21 +0100

    Add puzzleInput

---
+++ app/Classes/PuzzleInput.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Classes;
+
+class PuzzleInput
+{
+    static public function get(string $url):string{
+        
+        return "";
+    }
+}
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Classes\PuzzleInput;
 
 class PagesController extends Controller
 {
-    public function index(){ 
-        $puzzleInputText = "test";
-        $puzzleInputPlainText = strip_tags($puzzleInputText);
-        return view('pages.index')->with('puzzleInput', $puzzleInputPlainText);
-    } 
-    public function day1(){ return view('pages.day1');}
+    public function index(){return view('pages.index')->with('puzzleInput', "test");}
+    public function day1(){ return view('pages.day1')->with(['puzzleInput' => PuzzleInput::get("https://adventofcode.com/2021/day/1/input")]);}
     public function day2(){ return view('pages.day2');}
     public function day4(){ return view('pages.day4');}
-}    
+}
