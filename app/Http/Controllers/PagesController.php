49ee350455de1656cca80ea682e7d5e1e10a24b5
raoulkr <raoulkr@live.nl>
2021-12-30 14:14:43 +0100

    push Var in index page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function index(){ return view('pages.index');} 
+    public function index(){ 
+        $puzzleInputText = "test";
+        $puzzleInputPlainText = strip_tags($puzzleInputText);
+        return view('pages.index')->with('puzzleInput', $puzzleInputPlainText);
+    } 
     public function day1(){ return view('pages.day1');}
     public function day2(){ return view('pages.day2');}
     public function day4(){ return view('pages.day4');}
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h1>welcome to index</h1>
+    <p><?php echo $puzzleInput; ?></p>
 @endsection
