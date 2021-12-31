59fc62993518d6b51ccbbb710bdb787f680d3868
raoulkr <raoulkr@live.nl>
2021-12-31 14:33:18 +0100

    add puzzles to day pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,8 +7,8 @@ use App\Classes\PuzzleInput;
 
 class PagesController extends Controller
 {
-    public function index(){ return view('pages.index')->with(['puzzleInput' => PuzzleInput::get("puzzle1")]);}
+    public function index(){ return view('pages.index');}
     public function day1(){ return view('pages.day1')->with(['puzzleInput' => PuzzleInput::get("puzzle1")]);}
-    public function day2(){ return view('pages.day2');}
-    public function day4(){ return view('pages.day4');}
+    public function day2(){ return view('pages.day2')->with(['puzzleInput' => PuzzleInput::get("puzzle2")]);}
+    public function day4(){ return view('pages.day4')->with(['puzzleInput' => PuzzleInput::get("puzzle4")]);}
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h1>welcome</h1>
-    <p><?php echo $puzzleInput; ?></p>
+
 @endsection
