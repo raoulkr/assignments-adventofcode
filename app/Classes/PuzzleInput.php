e55053b09d30260f2a2e76f01a84ced227be2428
raoulkr <raoulkr@live.nl>
2021-12-31 02:12:36 +0100

    add PuzzleData

--- app/Classes/PuzzleInput.php
+++ app/Classes/PuzzleInput.php
@@ -4,8 +4,11 @@ namespace App\Classes;
 
 class PuzzleInput
 {
-    static public function get(string $url):string{
-        
-        return "";
+    static public function get(string $puzzle):string
+    {
+        $path = "puzzleData/";
+        $fileLocation = $path . $puzzle;
+        $puzzleInput = fopen($fileLocation, 'r');
+        return (string)$puzzleInput;
     }
 }
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,8 +7,8 @@ use App\Classes\PuzzleInput;
 
 class PagesController extends Controller
 {
-    public function index(){return view('pages.index')->with('puzzleInput', "test");}
-    public function day1(){ return view('pages.day1')->with(['puzzleInput' => PuzzleInput::get("https://adventofcode.com/2021/day/1/input")]);}
+    public function index(){ return view('pages.index')->with(['puzzleInput' => PuzzleInput::get("puzzle1")]);}
+    public function day1(){ return view('pages.day1')->with(['puzzleInput' => PuzzleInput::get("puzzle1")]);}
     public function day2(){ return view('pages.day2');}
     public function day4(){ return view('pages.day4');}
 }
