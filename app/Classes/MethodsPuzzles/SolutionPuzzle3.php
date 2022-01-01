770c5f42ee3f2c88a2e881e16c116d2fed440b8b
raoulkr <raoulkr@live.nl>
2022-01-01 18:58:41 +0100

    Add first attempt Puzzle Solution day3

--- app/Classes/MethodsPuzzles/SolutionPuzzle3.php
+++ app/Classes/MethodsPuzzles/SolutionPuzzle3.php
@@ -9,7 +9,45 @@ class SolutionPuzzle3
     static public function Solution(string $puzzle):string
     {
         PuzzleInput::get($puzzle);
+        $puzzleString = str_replace(array("\n", "\r"), ' ', PuzzleInput::get($puzzle));
+        $binaryData = explode(" ",$puzzleString);
+        $result = self::PowerConsumption($binaryData);
+        return "The PowerConsumption is: " . $result;
+    }
 
-        return "";
+    static private function PowerConsumption($binaryConsumptionData){
+        $zeroBit = array(0,0,0,0,0,0,0,0,0,0,0,0);
+        $oneBit = array(0,0,0,0,0,0,0,0,0,0,0,0);
+        $binary = array(0,0,0,0,0,0,0,0,0,0,0,0);
+
+        for ($i = 0; $i < count($binaryConsumptionData); $i++)
+		{
+            $singleBit = str_split($binaryConsumptionData[$i]);
+            for ($j = 0; $j < count($singleBit); $j++)
+            {
+                switch($singleBit[$j]){
+                    case "0":
+                        $zeroBit[$j]++;
+                        break;
+                    case "1":
+                        $oneBit[$j]++;
+                        break;
+                }
+            }
+        }
+
+        for ($i = 0; $i < count($zeroBit); $i++)
+		{
+            if($zeroBit[$i] < $oneBit[$i])
+            {
+                $binary[$i] = 1;
+            }else{
+                $binary[$i] = 0;
+            }
+        }
+
+        $powerConsumption = bindec(implode($binary));
+
+        return $powerConsumption;
     }
 }
